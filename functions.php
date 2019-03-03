1a5f026ff430089273a2291ea62829047233e2f7
Devin Dombrowski <dddesigns3@gmail.com>
2019-03-03 09:33:26 -0800

    w

--- functions.php
+++ functions.php
@@ -105,18 +105,13 @@ add_action( 'after_setup_theme', 'wrmd_blog_content_width', 0 );
  */
 function wrmd_blog_widgets_init() {
 	register_sidebar( array(
-		'name' => 'side-bar',
-        'before_widget' => '<div id="%1$s" class="%2$s panel panel-info">',
-        'before_title' => '<div class="panel-heading"><h3 class="panel-title">',
-        'after_title' => '</h3></div><div class="panel-body">',
-        'after_widget' => '</div></div>'
-		// 'name'          => esc_html__( 'Sidebar', 'wrmd-blog' ),
-		// 'id'            => 'sidebar-1',
-		// 'description'   => esc_html__( 'Add widgets here.', 'wrmd-blog' ),
-		// 'before_widget' => '<section id="%1$s" class="widget %2$s panel panel-info">',
-		// 'before_title' => '<div class="panel-heading"><h3 class="widget-title panel-title">',
-		// 'after_title' => '</h3></div><div class="panel-body">',
-		// 'after_widget'  => '</div></section>',
+		'name'          => esc_html__( 'Sidebar', 'wrmd-blog' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'wrmd-blog' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'wrmd_blog_widgets_init' );
