010d7f12464b609a6d65e25612a2bf328c7a522e
Devin Dombrowski <dddesigns3@gmail.com>
2019-03-03 10:09:29 -0800

    widget

--- functions.php
+++ functions.php
@@ -109,9 +109,9 @@ function wrmd_blog_widgets_init() {
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'wrmd-blog' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s panel panel-info">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title panel-title">',
-		'after_title'   => '</h2><div class="panel-body">',
+		'after_widget'  => '</div></section>',
+		'before_title'  => '<div class="panel-heading"><h3 class="widget-title panel-title">',
+		'after_title'   => '</h3></div><div class="panel-body">',
 
 		// 'name'          => esc_html__( 'Sidebar', 'wrmd-blog' ),
 		// 'id'            => 'sidebar-1',
