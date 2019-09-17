a71eb1fbe29fa3e643baa88b536ba7cfabe6c00e
Devin Dombrowski <dddesigns3@gmail.com>
2019-09-16 19:37:15 -0700

    disable primary-menu

--- header.php
+++ header.php
@@ -48,10 +48,10 @@
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wrmd-blog' ); ?></button>
 			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
+			// wp_nav_menu( array(
+			// 	'theme_location' => 'menu-1',
+			// 	'menu_id'        => 'primary-menu',
+			// ) );
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
