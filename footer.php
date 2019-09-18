d52e01c92fa30605b9cb0d0c02a6587f4a10fcd8
Devin Dombrowski <dddesigns3@gmail.com>
2019-09-17 19:39:49 -0700

    footer

--- footer.php
+++ footer.php
@@ -13,21 +13,63 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wrmd-blog' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wrmd-blog' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wrmd-blog' ), 'wrmd-blog', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="site-footer">
+	    <hr class="hr">
+	    <section class="bg-dusk">
+	        <div class="container">
+
+	            <div class="footer-supporting unit-1-1-mobile">
+	                <p>WRMD is proudly supporting the standards of</p>
+	                <div class="bg-light">
+	                    <div class="equidistant">
+	                        <ul>
+	                            <li>
+	                                <img src="https://www.wrmd.org/img/site/usfws.png" alt="U.S. Fish &amp; Wildlife Service">
+	                            </li>
+	                            <li>
+	                                <img src="https://www.wrmd.org/img/site/cadfw.png" alt="California Department of Fish &amp; Wildlife">
+	                            </li>
+	                            <li>
+	                                <img src="https://www.wrmd.org/img/site/nwra.png" alt="National Wildlife Rehabilitator's Association">
+	                            </li>
+	                            <li>
+	                                <img src="https://www.wrmd.org/img/site/iwrc.png" alt="International Wildlife Rehabilitation Council">
+	                            </li>
+	                        </ul>
+	                    </div>
+	                </div>
+
+	            </div>
+	            <div class="footer-social bg-night unit-1-1-mobile">
+	                <div class="equidistant">
+	                    <ul>
+	                        <li><a target="_blank" href="https://www.facebook.com/WildlifeRehabilitationMD">
+	                            <svg class="icon icon-facebook"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/symbol-defs.svg#icon-facebook"></use></svg>                        </a></li>
+	                        <li><a target="_blank" href="https://www.twitter.com/WildlifeRehabMD">
+	                            <svg class="icon icon-twitter"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/symbol-defs.svg#icon-twitter"></use></svg>                        </a></li>
+	                        <li><a target="_blank" href="https://www.youtube.com/user/WildlifeRehabMD">
+	                            <svg class="icon icon-youtube"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/symbol-defs.svg#icon-youtube"></use></svg>                        </a></li>
+	                        <li><a href="http://blog.wrmd.org/">
+	                            <svg class="icon icon-feed"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/symbol-defs.svg#icon-feed"></use></svg>                        </a></li>
+	                    </ul>
+	                </div>
+	            </div>
+	            <div class="footer-bread-crumbs unit-1-1-mobile">
+	                <nav>
+	                    <a href="https://www.wrmd.org">Home</a>
+	                    <a href="https://www.wrmd.org/about/terms-of-service">Terms and Policies</a>
+	                    <a href="http://help.wrmd.org">Help and FAQs</a>
+	                    <a href="http://blog.wrmd.org">Blog</a>
+	                    <a href="https://www.wrmd.org/about">About</a>
+	                    <a href="https://www.wrmd.org/about/contact">Contact Us</a>
+	                </nav>
+
+	                <p>Copyright © <a href="http://wildneighborsdp.org">The Wild Neighbors Database Project</a><br>
+	                    All rights reserved</p>
+	            </div>
+	        </div>
+	    </section>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
