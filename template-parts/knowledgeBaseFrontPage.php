6fd59eba9cd1aa462cc2209ab9baa0f32a31bbb8
Devin Dombrowski <dddesigns3@gmail.com>
2019-11-27 09:44:01 -0800

    add body

--- template-parts/knowledgeBaseFrontPage.php
+++ template-parts/knowledgeBaseFrontPage.php
@@ -1,7 +1,26 @@
 <?php /* Template Name: Knowledge Base Front Page */ ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<?php get_header(); ?>
 
-    <?php the_content(); ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+    <?php
+    while ( have_posts() ) :
+        the_post();
+    ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+        <?php the_content(); ?>
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // End of the loop. ?>
+
+    </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
