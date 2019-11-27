6ca462cdcc0ec3ee2aae4a94cf5cefc07e9afe8f
Devin Dombrowski <dddesigns3@gmail.com>
2019-11-27 09:38:08 -0800

    Knowledge Base Front Page theme

--- template-parts/knowledgeBaseFrontPage.php
+++ template-parts/knowledgeBaseFrontPage.php
@@ -1,1 +1,7 @@
 <?php /* Template Name: Knowledge Base Front Page */ ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+    <?php the_content(); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
