4bb216b0d12c2e1e74cc7801353c9fde88bc2edd
Devin Dombrowski <dddesigns3@gmail.com>
2019-11-27 16:08:54 -0800

    added Knowledge Base Article template

---
+++ template-parts/knowledgeBaseArticle.php
@@ -0,0 +1,26 @@
+<?php /* Template Name: Knowledge Base Article */ ?>
+
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+    <?php
+    while ( have_posts() ) :
+        the_post();
+    ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class('panel panel-default'); ?>>
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+        <?php the_content(); ?>
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // End of the loop. ?>
+
+    </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
